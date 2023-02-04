5360cc24b1493802242113b9fe7027a72f60ad1f
Ramon Leonardo <ramon.c.leo@hotmail.com>
2023-02-04 12:28:13 -0300

    Organizando estrutura de pasta e arquivos em signup

--- pages/auth/signup/index.js
+++ pages/auth/signup/index.js
@@ -10,39 +10,11 @@ import {
   InputLabel,
   Typography
 } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
 
 import { initialValues, validationSchema } from './formValues'
-import TemplateDefault from '../../src/templates/Default'
+import TemplateDefault from '../../../src/templates/Default'
 
-const useStyles = makeStyles((theme) => ({
-  boxContainer: {
-    paddingBottom: theme.spacing(4),
-  },
-
-  box: {
-    backgroundColor: theme.palette.background.white,
-    padding: theme.spacing(3),
-  },
-
-  inputLabel: {
-    fontWeight: 400,
-    color: theme.palette.primary.main,
-  },
-
-  formControl:{
-    marginBottom: theme.spacing(1),
-  },
-
-  boxButton: {
-    marginBottom: theme.spacing(2),
-  },
-
-  button: {
-    marginBottom: theme.spacing(1),
-  }
-
-}))
+import useStyles from './styles'
 
 const Signup = () => {
 
---
+++ pages/auth/signup/styles.js
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  boxContainer: {
+    paddingBottom: theme.spacing(4),
+  },
+
+  box: {
+    backgroundColor: theme.palette.background.white,
+    padding: theme.spacing(3),
+  },
+
+  inputLabel: {
+    fontWeight: 400,
+    color: theme.palette.primary.main,
+  },
+
+  formControl:{
+    marginBottom: theme.spacing(1),
+  },
+
+  boxButton: {
+    marginBottom: theme.spacing(2),
+  },
+
+  button: {
+    marginBottom: theme.spacing(1),
+  }
+
+}))
+
+export default useStyles
